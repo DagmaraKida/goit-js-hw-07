6338fc4f040c090b2666eb15f5df862f026eda47
Dagmara Kida <dagmarakida@gmail.com>
2023-06-27 18:12:01 +0100

    task 02

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,31 @@ import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryElement = document.querySelector("ul.gallery");
+
+const galleryItemsElements = galleryItems
+  .map(
+    ({ preview, original, description }) => `
+      
+        <li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                    class="gallery__image"
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"
+                />
+            </a>
+        </li>
+      
+    `
+  )
+  .join("");
+
+galleryElement.insertAdjacentHTML("beforeend", galleryItemsElements);
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
